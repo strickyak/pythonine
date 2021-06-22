0f6212a7633044ebcafe9194fcedbad3b2162857
strick <strick@yak.net>
2021-06-22 08:55:42 -0700

    more asserts in test2 to check branches.

--- test2.py
+++ test2.py
@@ -4,18 +4,25 @@ z = x + y - 205
 assert 0 == z
 if z:
     print z
+    assert 0
 else:
     print 42  # <-- 42
+    assert 1
 if x:
     print x  # <-- 100
+    assert 1
 else:
     print 13
+    assert 0
 if z:
     print z
+    assert 0
 elif y:
     print y  # <-- 105
+    assert 1
 else:
     print 13
+    assert 0
 
 sum = 0
 i = 0
