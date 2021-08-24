6665211828602c2f16aca5efd98b15d63deec031
strick <strick@yak.net>
2021-08-24 15:06:16 -0700

    TEST___JUST_FOR_EXAMINING_THE_ASSEMBLY_OUTPUT

--- defs.c
+++ defs.c
@@ -63,4 +63,61 @@ void defs_init(void (*marker_fn)()) {
   oinit(data, data_end, marker_fn);
   odump(0, 0, 0, 0);
 }
+
 #endif  // ========================================
+
+#ifdef TEST___JUST_FOR_EXAMINING_THE_ASSEMBLY_OUTPUT
+
+int xxx;
+
+struct Pair {
+    word up;
+    word down;
+};
+
+struct HiLo {
+    byte hi;
+    byte lo;
+};
+
+word TestWord(word a, word b) {
+    word c;
+    c = 1;
+    {
+        word d;
+        d = 4 + a;
+        xxx += d;
+    }
+    {
+        word e;
+        e = 6 + b;
+        xxx += e;
+    }
+    return a+b+c;
+}
+
+byte TestByte(byte a, byte b) {
+    byte c;
+    c = 1;
+    {
+        byte d;
+        d = 4 + a;
+        xxx += d;
+        printf("%d", d);
+        printf("%d", &d);
+    }
+    {
+        byte e;
+        e = 6 + b;
+        xxx += e;
+        printf("%d", e);
+        printf("%d", &e);
+    }
+    return a+b+c;
+}
+
+struct Pair TestPair(struct Pair a, struct Pair b) {
+    struct Pair foo = {a.up+a.up+3, b.down+b.down+5};
+    return foo;
+}
+#endif
