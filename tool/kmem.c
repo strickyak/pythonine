0590ad431c63b95c5463ab0d9361a29b82fe9590
strick <strick@yak.net>
2021-09-28 18:14:44 -0700

    kmem mapping experiments.

--- tool/kmem.c
+++ tool/kmem.c
@@ -1,10 +1,17 @@
 #include <cmoc.h>
+#include <assert.h>
 
+#define NUM_BLOCKS 32
+
+#define F_AllRAM 0x39
 #define F_GBlkMp 0x19
+#define F_ClrBlk 0x50
+#define F_MapBlk 0x4F
 
 typedef unsigned char byte;
 typedef unsigned int word;
 
+byte BlockNum[NUM_BLOCKS];
 byte Buffer[1024];
 
 void Disable() {
@@ -19,12 +26,16 @@ void Enable() {
   }
 }
 
-byte AllocateRam(byte num_blks, word* start_blk_out) {
+void Poke(word addr, byte val) {
+    *(byte*)addr = val;
+}
+
+byte AllocateBlock(byte num_blks, word* start_blk_out) {
   byte err = 0;
   asm {
     PSHS Y,U
     LDB num_blks
-    os9 $39
+    os9 F_AllRAM
     PULS Y,U
     bcc AR__OKAY
 
@@ -47,7 +58,7 @@ byte MapBlock(int start_blk, byte num_blks, word* addr_out) {
     PSHS Y,U
     LDB num_blks
     LDX start_blk
-    os9 $4F
+    os9 F_MapBlk
     TFR U,X
     PULS Y,U
     bcc MB__OKAY
@@ -60,6 +71,26 @@ MB__OKAY
     stx [addr_out]
 
 MB__RET
+    nop
+  }
+  return err;
+}
+byte UnMapBlock(word addr, byte num_blks) {
+  byte err = 0;
+  asm {
+    PSHS Y,U
+    LDB num_blks
+    LDU addr
+    os9 F_ClrBlk
+    PULS Y,U
+    bcc UMB__OKAY
+
+UMB__BAD
+    stb err
+    bra UMB__RET
+
+UMB__OKAY
+UMB__RET
     nop
   }
   return err;
@@ -99,9 +130,9 @@ void PrintBlockMap() {
   int err = GetBlockMap(Buffer, &bpb, &num_blks);
   if (err) { printf("ERROR %d\n", err); return; }
 
-  printf("bpb: %u\n", bpb);
-  printf("num_blks: %u\n", num_blks);
-  for (int i=0; i < 256; i+=16) {
+  printf("bpb: %u num_blks: %u\n", bpb, num_blks);
+  for (int i=0; i < (num_blks+15); i+=16) {
+    printf("%04x:", i);
     for (int j=0; j<16; j++) {
       printf(" %02x", Buffer[i+j]);
     }
@@ -110,19 +141,63 @@ void PrintBlockMap() {
 }
 
 int main() {
+  printf("main=$%04x\n", (unsigned)(void*)main);
   PrintBlockMap();
 
-  for (int i=0; i < 16; i++) {
+  for (int i=0; i < NUM_BLOCKS; i++) {
     word start_blk, addr;
     printf("allocate ram[%d] ... ", i);
-    byte err = AllocateRam(1, &start_blk);
+    byte err = AllocateBlock(1, &start_blk);
     if (err) { printf("ERROR %d\n", err); return err; }
+    BlockNum[i] = start_blk;
     printf("=> blk %d; ", start_blk);
     err = MapBlock(start_blk, 1, &addr);
-    if (err) { printf("ERROR %d\n", err); }
-    printf(" MAP addr => %x\n", addr);
+    printf(" MAP addr => %x; error %d\n", addr, err);
     PrintBlockMap();
+
+    if (!err && (i%3)>0) {
+        err = UnMapBlock(addr, 1);
+        printf(" UNMAP addr: %x ; error %d\n", addr, err);
+        PrintBlockMap();
+    }
   }
 
+  printf("Part Two\n");
+  for (word addr = 0x2000; addr < 0xE000; addr += 0x2000) {
+        byte err = UnMapBlock(addr, 1);
+        printf(" UNMAP addr: %x ; error %d\n", addr, err);
+  }
+
+  printf("Part Three\n");
+  for (word k = 0; k < NUM_BLOCKS; k++) {
+        byte b = BlockNum[k];
+        word addr;
+        byte err = MapBlock(b, 1, &addr);
+        printf("k=%d MAP start: %d addr: %x ; error %d\n", k, b, addr, err);
+        *(byte*)addr = 0xDE ^ b;
+        err = UnMapBlock(addr, 1);
+        printf("...  UNMAP addr: %x ; error %d\n", addr, err);
+  }
+
+  printf("Part Four\n");
+  for (word k = 0; k < NUM_BLOCKS; k++) {
+        byte b = BlockNum[k];
+        word addr;
+        byte err = MapBlock(b, 1, &addr);
+        printf("k=%d MAP start: %d addr: %x ; error %d\n", k, b, addr, err);
+        assert( *(byte*)addr == 0xDE ^ b);
+        err = UnMapBlock(addr, 1);
+        printf("...  UNMAP addr: %x ; error %d\n", addr, err);
+  }
+
+  printf("Part Five\n");
+  for (word k = 0; k < NUM_BLOCKS; k++) {
+        byte b = BlockNum[k];
+        printf("POKE: k=%d MAP start: %d \n", k, b );
+        Disable();
+        Poke(0xFFA0 + 8 + 6, b);
+        assert( *(byte*)0xC000 == 0xDE ^ b);
+        Enable();
+  }
   return 0;
 }
