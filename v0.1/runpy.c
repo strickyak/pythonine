04bb414e63e74ab8d1a5b114fa3ad98492d3a3b7
strick <strick@yak.net>
2022-05-09 09:00:00 -0700

    rynpy always asks for $8000 data memory.

--- v0.1/runpy.c
+++ v0.1/runpy.c
@@ -5,7 +5,12 @@
 #include "readbuf.h"
 #include "runtime.h"
 
-int main(int argc, char* argv[]) {
+#if !unix
+#include "os9errno.h"
+#include "os9.h"
+#endif
+
+void main2() {
 #if !unix
   // printf("(main) $%x=%d. ", main, main);
 #endif
@@ -24,5 +29,62 @@ int main(int argc, char* argv[]) {
   EvalCodes(main_bytecodes);
   ogc();
   odumpsummary();
-  return 0;
+  exit(0);
+}
+
+int main(int argc, char* argv[]) {
+#if !unix
+  word old_size = 0;
+  word wanted_size = 0x8000;
+  word new_size = 0;
+  byte err = 0;
+  word stack_pointer = 0;
+
+  asm {
+    sts stack_pointer
+
+    leay 0,y  ; sets Z flag.  Zero means Level 2 OS-9.
+    beq IsLevel2
+
+    ldb #E_BMODE   ; Cannot work on Level 1 OS-9.
+    bra ReSizeError
+
+IsLevel2
+    clra  ; non means query
+    clrb
+    pshs y,u
+    swi2
+    fcb   F_MEM
+    puls y,u
+    bcs ReSizeError
+    std old_size
+
+    ldd wanted_size  ; nonzero means desire.
+    pshs y,u
+    swi2
+    fcb   F_MEM
+    puls y,u
+    bcs ReSizeError
+    std new_size
+    bra ReSizeOk
+
+ReSizeError
+    stb err
+ReSizeOk
+  }
+  printf("err %d. old %x new %x\n", (int)err, old_size, new_size);
+  if (err) exit(err);
+
+  asm {
+    tfr y,d     ; zero in D, X, and Y
+    tfr y,x
+    lds #$8000  ; Start new high stack
+    pshs d,x,y
+    pshs d,x,y
+    tfr s,u     ; Start new high frame pointer
+    pshs d,x,y
+    pshs d,x,y
+  }
+#endif
+  main2();
 }
